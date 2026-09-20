f0a5bce4d26fa338037c92a5eb435c777b346ed3
Orpheus9189 <orpheus9189@gmail.com>
2026-09-20 13:58:00 -0400

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public Class Main {
+    public static void main [String[] args] {
+            int intOperandA = 1;
+            int intOperandB = 3;
+            int inSum = 7;
+            int intProduct = 9;
+            int intDifference = 12;
+            int intQuotient = 14;
+            int intModulo = 17
+
+            double doubleOperandA = 3.25;
+            double doubleOperandB = 4.29;
+            double doubleSum = 5.51;
+            double doubleProduct = 7.56;
+            double doubleDifferance = 8.29;
+            double doubleQuotient = 11.5;
+
+            intSum = intOperandA + intOperandB;
+            System.out.println("The sum using ints of " + intOperandA intSum);
+        }
+        }
